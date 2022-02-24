b4362631b8b98b1fe16961e6389b70a12cb5d109
ErickHidalgo9 <99521248+ErickHidalgo9@users.noreply.github.com>
2022-02-23 22:56:46 -0600

    Update TarjetaDAOImpl.java

--- APITarjetaCredito/src/main/java/com/ibm/academia/restapi/tarjeta/servicios/TarjetaDAOImpl.java
+++ APITarjetaCredito/src/main/java/com/ibm/academia/restapi/tarjeta/servicios/TarjetaDAOImpl.java
@@ -1,5 +1,7 @@
 package com.ibm.academia.restapi.tarjeta.servicios;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,14 +10,17 @@ import com.ibm.academia.restapi.tarjeta.modelo.entidades.Tarjeta;
 import com.ibm.academia.restapi.tarjeta.repositorios.TarjetaRepository;
 
 @Service
-public class TarjetaDAOImpl extends GenericoDAOImpl<Tarjeta, TarjetaRepository> implements TarjetaDAO{
+public class TarjetaDAOImpl implements TarjetaDAO { 
 
 	@Autowired
-	public TarjetaDAOImpl(TarjetaRepository repository) {
-		super(repository);
-	}
-
-	@Override
+	TarjetaRepository repository;
+	
+	@Transactional
+	 public List<Tarjeta> list(){
+	        return repository.findAll();
+	    }
+	
+	
 	@Transactional(readOnly = true)
 	public Iterable<Tarjeta> findByPreferencia(String preferencia) {
 		
