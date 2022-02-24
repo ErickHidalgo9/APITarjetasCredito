6f8b55e70e0d833bfd9cff8985c4d5cd90be9088
ErickHidalgo9 <99521248+ErickHidalgo9@users.noreply.github.com>
2022-02-23 22:54:20 -0600

    Update TarjetaRepository.java

--- APITarjetaCredito/src/main/java/com/ibm/academia/restapi/tarjeta/repositorios/TarjetaRepository.java
+++ APITarjetaCredito/src/main/java/com/ibm/academia/restapi/tarjeta/repositorios/TarjetaRepository.java
@@ -2,16 +2,17 @@ package com.ibm.academia.restapi.tarjeta.repositorios;
 
 import java.util.List;
 
-//import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.stereotype.Repository;
 
 import com.ibm.academia.restapi.tarjeta.modelo.entidades.Tarjeta;
 
 @Repository
-public interface TarjetaRepository extends CrudRepository<Tarjeta, Long>{
+public interface TarjetaRepository extends JpaRepository<Tarjeta, Long>{
 	
 	public List<Tarjeta> findByPreferencia(String preferencia);
 	
 
 }
+
