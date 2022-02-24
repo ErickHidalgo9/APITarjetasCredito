43f11d3403b293cbbe0c7045476e29dc745d543b
ErickHidalgo9 <99521248+ErickHidalgo9@users.noreply.github.com>
2022-02-23 22:56:05 -0600

    Update TarjetaDAO.java

--- APITarjetaCredito/src/main/java/com/ibm/academia/restapi/tarjeta/servicios/TarjetaDAO.java
+++ APITarjetaCredito/src/main/java/com/ibm/academia/restapi/tarjeta/servicios/TarjetaDAO.java
@@ -1,9 +1,12 @@
 package com.ibm.academia.restapi.tarjeta.servicios;
 
+import java.util.List;
+
 import com.ibm.academia.restapi.tarjeta.modelo.entidades.Tarjeta;
 
-public interface TarjetaDAO extends GenericoDAO<Tarjeta> {
+public interface TarjetaDAO { 
 	
+	public List<Tarjeta> list();
 	public Iterable<Tarjeta> findByPreferencia(String preferencia);
 	
 
